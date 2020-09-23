0de2f00b0c5ce562ab94795e579c63b3b0c9ea54
NavarroJ <johnjnavarro94@gmail.com>
2020-09-23 13:54:44 -0400

    lab two

---
+++ labtwo/AminoQuiz.java
@@ -0,0 +1,24 @@
+package labtwo;
+
+public class AminoQuiz {
+
+	public static void main(String[] args) {
+		
+		String[] SHORT_NAMES = 
+			{ "A","R", "N", "D", "C", "Q", "E", 
+			"G",  "H", "I", "L", "K", "M", "F", 
+			"P", "S", "T", "W", "Y", "V" };
+		
+		String[] FULL_NAMES = 
+			{
+			"alanine","arginine", "asparagine", 
+			"aspartic acid", "cysteine",
+			"glutamine",  "glutamic acid",
+			"glycine" ,"histidine","isoleucine",
+			"leucine",  "lysine", "methionine", 
+			"phenylalanine", "proline", 
+			"serine","threonine","tryptophan", 
+			"tyrosine", "valine"};
+	}
+
+}
