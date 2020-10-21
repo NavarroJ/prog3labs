edf98c627c44de095d4c3a730e4d43b6938a7d27
NavarroJ <johnjnavarro94@gmail.com>
2020-10-21 14:14:12 -0400

    lab 5

--- labfive/TestSVGUI.java
+++ labfive/TestSVGUI.java
@@ -1,10 +1,15 @@
 package labfive;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
 
 public class TestSVGUI extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
+	
+//	private JMenuBar myMenuBar() {
+//		
+//	}
 
 	public TestSVGUI(String title) {
 		super(title);
@@ -13,6 +18,7 @@ public class TestSVGUI extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
+
 
 	public static void main(String[] args) {
 		new TestSVGUI("Test 1");
